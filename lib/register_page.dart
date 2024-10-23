f040be7a27eaa88cd9f7955678956e78a1c3d9e9
TEUNGKU-ZULKIFLI <teungkuzulkifli08@gmail.com>
2024-10-23 12:00:00 +0700

    menambahkan button login di page register

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart'; // Import halaman Login
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -10,6 +11,27 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Register Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Belum punya akun? Silakan daftar!'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigasi ke halaman Login
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Text('Sudah punya akun? Login di sini'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
