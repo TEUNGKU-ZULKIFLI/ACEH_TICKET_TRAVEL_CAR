fb13f5a248524820e58f397b83dcfbcdca777b58
TEUNGKU-ZULKIFLI <teungkuzulkifli08@gmail.com>
2024-10-24 16:07:24 +0700

    MERESET KE TITIK ULANG 😊

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,76 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-// Inisialisasi Plugin Notifikasi
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Travel App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   final List<Map<String, String>> tickets = [
     {
       'image': 'assets/img/hiace_modern.png',
-      'title': 'Travel Lhoksemawe-Banda Aceh',
-      'price': 'Rp 150.000'
+      'title': 'Travel ke Kota A',
+      'price': 'Rp 100.000'
     },
     {
       'image': 'assets/img/hiace_coklat.png',
-      'title': 'Travel Lhoksemawe-Medan',
-      'price': 'Rp 150.000'
+      'title': 'Travel ke Kota B',
+      'price': 'Rp 120.000'
     },
     {
       'image': 'assets/img/hiace_putih.png',
-      'title': 'Travel Lhoksemawe-Langsa',
-      'price': 'Rp 60.000'
+      'title': 'Travel ke Kota C',
+      'price': 'Rp 150.000'
     },
   ];
-
-  // Daftar tiket favorit
-  List<Map<String, String>> favoriteTickets = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Daftar Tiket Trevel.com'),
+        title: Text('Daftar Tiket Yusuf'),
         backgroundColor: Colors.blue[800],
-        actions: [
-          IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => FavoriteScreen(favoriteTickets: favoriteTickets),
-                ),
-              );
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.filter_list),
-            onPressed: _showFilterDialog,
-          ),
-        ],
       ),
       body: Container(
         child: ListView.builder(
@@ -91,9 +45,6 @@ class _HomeScreenState extends State<HomeScreen> {
                     width: 80,
                     height: 80,
                     fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Image.asset('assets/img/placeholder.png', fit: BoxFit.cover);
-                    },
                   ),
                 ),
                 title: Text(
@@ -104,37 +55,17 @@ class _HomeScreenState extends State<HomeScreen> {
                   ticket['price']!,
                   style: TextStyle(fontSize: 16, color: Colors.green),
                 ),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.favorite_border),
-                      onPressed: () {
-                        setState(() {
-                          favoriteTickets.add(ticket);
-                        });
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Tiket ditambahkan ke favorit!')),
-                        );
-                      },
+                trailing: ElevatedButton(
+                  onPressed: () {
+                    // Tambahkan aksi saat tombol ditekan
+                  },
+                  child: Text('Pesan'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue[800],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigasi ke halaman detail atau fungsi pemesanan
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => DetailScreen(ticket: ticket)),
-                        );
-                      },
-                      child: Text('Pesan'),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue[800],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             );
@@ -143,147 +74,4 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-
-  // Fungsi Filter Tiket
-  void _showFilterDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Filter Tiket'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                title: Text('Harga Terendah'),
-                onTap: () {
-                  setState(() {
-                    tickets.sort((a, b) => int.parse(a['price']!.replaceAll(RegExp(r'[Rp .]'), '')).compareTo(
-                        int.parse(b['price']!.replaceAll(RegExp(r'[Rp .]'), ''))));
-                  });
-                  Navigator.of(context).pop();
-                },
-              ),
-              ListTile(
-                title: Text('Harga Tertinggi'),
-                onTap: () {
-                  setState(() {
-                    tickets.sort((a, b) => int.parse(b['price']!.replaceAll(RegExp(r'[Rp .]'), '')).compareTo(
-                        int.parse(a['price']!.replaceAll(RegExp(r'[Rp .]'), ''))));
-                  });
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
-
-// Halaman Favorit Tiket
-class FavoriteScreen extends StatelessWidget {
-  final List<Map<String, String>> favoriteTickets;
-
-  FavoriteScreen({required this.favoriteTickets});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tiket Favorit'),
-      ),
-      body: ListView.builder(
-        itemCount: favoriteTickets.length,
-        itemBuilder: (context, index) {
-          final ticket = favoriteTickets[index];
-          return ListTile(
-            leading: Image.asset(ticket['image']!),
-            title: Text(ticket['title']!),
-            subtitle: Text(ticket['price']!),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  final Map<String, String> ticket;
-
-  DetailScreen({required this.ticket});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(ticket['title']!),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(ticket['image']!),
-            SizedBox(height: 20),
-            Text(ticket['title']!, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            SizedBox(height: 10),
-            Text('Harga: ${ticket['price']!}', style: TextStyle(fontSize: 20, color: Colors.green)),
-            SizedBox(height: 20),
-            Text('Deskripsi:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            Text('Perjalanan dari Lhoksemawe ke Banda Aceh dengan Aman & Nyaman.'),
-            SizedBox(height: 20),
-            Text('Fasilitas:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            Text('AC, Kursi Nyaman'),
-            SizedBox(height: 20),
-            Text('Opsi Pembayaran:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            Text('Transfer Bank, Kartu Kredit, E-Wallet'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Konfirmasi Pemesanan anda'),
-                      content: Text('Apakah Anda yakin ingin memesan tiket ini?'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(); // Tutup dialog
-                          },
-                          child: Text('Batal'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            // Logika untuk menyelesaikan pemesanan
-                            Navigator.of(context).pop(); // Tutup dialog
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('Pemesaanan Tiket berhasil dipesan!')),
-                            );
-                          },
-                          child: Text('Pesan'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Text('Pesan Tiket'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue[800],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,305 +1,86 @@
 import 'package:flutter/material.dart';
+import 'bottom_nav_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Auth',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginPage(),
-    );
-  }
-}
-
-// Halaman Login
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  // Variabel untuk menyimpan email dan password yang didaftarkan
-  String? registeredEmail;
-  String? registeredPassword;
-
-  // Fungsi untuk menerima email dan password dari halaman SignUpPage
-  void registerAccount(String email, String password) {
-    setState(() {
-      registeredEmail = email;
-      registeredPassword = password;
-    });
-  }
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _isObscure = true;
 
   void _login() {
-    final email = _emailController.text;
-    final password = _passwordController.text;
+    if (_formKey.currentState!.validate()) {
+      String username = _usernameController.text;
+      String password = _passwordController.text;
 
-    if (email == registeredEmail && password == registeredPassword) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => HomePage(
-            email: registeredEmail!,
-          ),
-        ),
-      );
-    } else {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return const AlertDialog(
-            title: Text('Login Failed'),
-            content: Text('Invalid email or password.'),
-          );
-        },
-      );
+      // Simulasi autentikasi
+      if (username == 'admin' && password == '1') {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => BottomNavPage()),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Username atau password salah')),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
+      appBar: AppBar(title: Text('Login Page')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Harap masukkan username';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: _login,
-              child: const Text('Login'),
-            ),
-            const SizedBox(height: 10.0),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SignUpPage(onSignUp: registerAccount),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                        _isObscure ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
                   ),
-                );
-              },
-              child: const Text("Don't have an account? Sign up"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// Halaman Registrasi (Daftar Akun)
-class SignUpPage extends StatefulWidget {
-  final Function(String email, String password) onSignUp;
-
-  const SignUpPage({Key? key, required this.onSignUp}) : super(key: key);
-
-  @override
-  _SignUpPageState createState() => _SignUpPageState();
-}
-
-class _SignUpPageState extends State<SignUpPage> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  final _confirmPasswordController = TextEditingController();
-
-  void _signUp() {
-    final email = _emailController.text;
-    final password = _passwordController.text;
-    final confirmPassword = _confirmPasswordController.text;
-
-    if (password == confirmPassword) {
-      // Mengirim email dan password ke halaman login melalui callback
-      widget.onSignUp(email, password);
-      Navigator.pop(context);
-    } else {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return const AlertDialog(
-            title: Text('Sign Up Failed'),
-            content: Text('Passwords do not match.'),
-          );
-        },
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
+                ),
+                obscureText: _isObscure,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Harap masukkan password';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _login,
+                child: Text('Login'),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _confirmPasswordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Confirm Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: _signUp,
-              child: const Text('Sign Up'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// Halaman Beranda setelah login
-class HomePage extends StatelessWidget {
-  final String email;
-
-  const HomePage({Key? key, required this.email}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              // Logout dan kembali ke halaman login
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const LoginPage()),
-              );
-            },
-            icon: const Icon(Icons.logout),
+            ],
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            Text(
-              'Welcome, $email!',
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Menu Options:',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('Profile'),
-              onTap: () {
-                // Navigasi ke halaman Profile
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ProfilePage()),
-                );
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                // Navigasi ke halaman Settings
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.help),
-              title: const Text('Help'),
-              onTap: () {
-                // Navigasi ke halaman Help
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// Halaman Profil
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: const Center(
-        child: Text(
-          'This is the Profile Page.',
-          style: TextStyle(fontSize: 24),
         ),
       ),
     );
