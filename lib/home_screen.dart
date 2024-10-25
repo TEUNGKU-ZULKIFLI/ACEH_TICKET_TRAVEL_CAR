f8c80b3db2dcab77c68cc3f781120e3bb937d2b0
muhammad yusuf <acehcayberteam32@gmail.com>
2024-10-25 08:05:48 +0700

    yusuf membuat beberapa fitur page home screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,27 +9,34 @@ class _HomeScreenState extends State<HomeScreen> {
   final List<Map<String, String>> tickets = [
     {
       'image': 'assets/img/hiace_modern.png',
-      'title': 'Travel Lhoksemawe-Banda Aceh',
-      'price': 'Rp 150.000'
+      'title': 'Mobil-Hiace',
+      'price': 'Rp 100.000'
+      
     },
     {
       'image': 'assets/img/hiace_coklat.png',
-      'title': 'Travel Lhoksemawe-Medan',
+      'title': 'Travel ke Kota B',
       'price': 'Rp 120.000'
     },
     {
       'image': 'assets/img/hiace_putih.png',
-      'title': 'Travel Lhoksemawe-Langsa',
-      'price': 'Rp 60.000'
+      'title': 'Travel ke Kota C',
+      'price': 'Rp 150.000'
     },
   ];
 
   String searchQuery = '';
   String selectedOriginCity = 'Pilih Kota Asal';
   String selectedDestinationCity = 'Pilih Kota Tujuan';
-  int selectedSeats = 1;
+  int selectedSeats = 1; // Jumlah kursi default
 
-  final List<String> cities = ['Kota Lhoksemawe', 'Kota Lhoksukon', 'Sigli', 'Perulak', 'Idi'];
+  final List<String> cities = [
+    'Kota Lhoksemawe',
+    'Kota Lhoksukon',
+    'Sigli',
+    'Perulak',
+    'Idi'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -39,13 +46,15 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Daftar Tiket Trevel Aceh.com'),
+        title: Text('Daftar Tiket Yusuf'),
         backgroundColor: Colors.blue[800],
       ),
       body: Padding(
         padding: EdgeInsets.all(10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Pencarian Tiket
             TextField(
               onChanged: (value) {
                 setState(() {
@@ -70,13 +79,15 @@ class _HomeScreenState extends State<HomeScreen> {
                   selectedOriginCity = newValue!;
                 });
               },
-              items: cities.map<DropdownMenuItem<String>>((String city) {
+              items: ['Pilih Kota Asal', ...cities].map<DropdownMenuItem<String>>((String city) {
                 return DropdownMenuItem<String>(
                   value: city,
                   child: Text(city),
                 );
               }).toList(),
             ),
+
+            SizedBox(height: 10),
 
             // Dropdown untuk memilih kota tujuan
             DropdownButton<String>(
@@ -86,13 +97,15 @@ class _HomeScreenState extends State<HomeScreen> {
                   selectedDestinationCity = newValue!;
                 });
               },
-              items: cities.map<DropdownMenuItem<String>>((String city) {
+              items: ['Pilih Kota Tujuan', ...cities].map<DropdownMenuItem<String>>((String city) {
                 return DropdownMenuItem<String>(
                   value: city,
                   child: Text(city),
                 );
               }).toList(),
             ),
+
+            SizedBox(height: 10),
 
             // Pilihan jumlah kursi
             Row(
@@ -118,6 +131,73 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
 
+            SizedBox(height: 10),
+
+            // Gambar iklan mobil
+            Container(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: tickets.length,
+                itemBuilder: (context, index) {
+                  final ticket = tickets[index];
+                  return Container(
+                    width: 160,
+                    margin: EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 5,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                      color: Colors.white,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Stack(
+                        children: [
+                          Image.asset(
+                            ticket['image']!,
+                            width: 160,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              color: Colors.black54,
+                              padding: EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    ticket['title']!,
+                                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    ticket['price']!,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            // Daftar Tiket
             Expanded(
               child: ListView.builder(
                 itemCount: filteredTickets.length,
@@ -129,13 +209,47 @@ class _HomeScreenState extends State<HomeScreen> {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.asset(
-                        ticket['image']!,
-                        width: double.infinity,
-                        height: 150,
-                        fit: BoxFit.cover,
+                    child: ListTile(
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(
+                          ticket['image']!,
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      title: Text(
+                        ticket['title']!,
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        ticket['price']!,
+                        style: TextStyle(fontSize: 16, color: Colors.green),
+                      ),
+                      trailing: ElevatedButton(
+                        onPressed: () {
+                          // Navigasi ke layar pemesanan tiket
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => BookingScreen(
+                                title: ticket['title']!,
+                                price: ticket['price']!,
+                                originCity: selectedOriginCity,
+                                destinationCity: selectedDestinationCity,
+                                seats: selectedSeats,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Text('Pesan'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue[800],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
                       ),
                     ),
                   );
@@ -145,46 +259,20 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 10.0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {
-                // Aksi untuk kembali ke halaman utama
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BookingScreen(
-                      originCity: selectedOriginCity,
-                      destinationCity: selectedDestinationCity,
-                      seats: selectedSeats,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
 
 class BookingScreen extends StatelessWidget {
+  final String title;
+  final String price;
   final String originCity;
   final String destinationCity;
   final int seats;
 
   BookingScreen({
+    required this.title,
+    required this.price,
     required this.originCity,
     required this.destinationCity,
     required this.seats,
@@ -202,63 +290,27 @@ class BookingScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Kota Asal: $originCity'),
-            Text('Kota Tujuan: $destinationCity'),
-            Text('Jumlah Kursi: $seats'),
+            Text(
+              'Detail Pemesanan',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text('Judul: $title', style: TextStyle(fontSize: 18)),
+            Text('Harga: $price', style: TextStyle(fontSize: 18)),
+            Text('Kota Asal: $originCity', style: TextStyle(fontSize: 18)),
+            Text('Kota Tujuan: $destinationCity', style: TextStyle(fontSize: 18)),
+            Text('Jumlah Kursi: $seats', style: TextStyle(fontSize: 18)),
             SizedBox(height: 20),
             Center(
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PaymentScreen(),
-                    ),
-                  );
+                  // Tindakan ketika tombol 'Pesan Sekarang' ditekan
+                  _showConfirmationDialog(context);
                 },
-                child: Text('Lanjut ke Pembayaran'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class PaymentScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pembayaran'),
-        backgroundColor: Colors.blue[800],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Metode Pembayaran:', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            ListTile(
-              leading: Icon(Icons.payment),
-              title: Text('Transfer Bank'),
-            ),
-            ListTile(
-              leading: Icon(Icons.credit_card),
-              title: Text('Kartu Kredit/Debit'),
-            ),
-            ListTile(
-              leading: Icon(Icons.money),
-              title: Text('Bayar di Tempat'),
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  _showPaymentSuccessDialog(context);
-                },
-                child: Text('Bayar Sekarang'),
+                child: Text('Pesan Sekarang'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[800],
+                ),
               ),
             ),
           ],
@@ -267,21 +319,47 @@ class PaymentScreen extends StatelessWidget {
     );
   }
 
-  void _showPaymentSuccessDialog(BuildContext context) {
+  void _showConfirmationDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("Pembayaran Sukses"),
-          content: Text('Pembayaran berhasil! Terima kasih telah memesan tiket.'),
+          title: Text("Konfirmasi Pemesanan"),
+          content: Text('Apakah Anda yakin ingin memesan tiket ini?'),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
+              },
+              child: Text("Batal"),
+            ),
+            TextButton(
+              onPressed: () {
+                // Logika pemesanan tiket bisa ditambahkan di sini
                 Navigator.of(context).pop();
+                _showSuccessDialog(context);
+              },
+              child: Text("Konfirmasi"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showSuccessDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Pemesanan Berhasil"),
+          content: Text('Tiket Anda telah dipesan!'),
+          actions: [
+            TextButton(
+              onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('OK'),
+              child: Text("OK"),
             ),
           ],
         );
