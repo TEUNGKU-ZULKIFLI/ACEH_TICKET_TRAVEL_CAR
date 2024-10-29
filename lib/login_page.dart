1b58210de3bbefc8438ce92e36621d4f0f1fb9e5
Muhammad Rizana Fajri <muhammadrizanafajri@gmail.com>
2024-10-29 14:28:36 +0700

    fajri membuat login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -34,53 +34,239 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login Page')),
-      body: Padding(
+      appBar: AppBar(
+        title: Text('Login Page'),
+        backgroundColor: Colors.blueAccent, // Warna latar belakang AppBar
+      ),
+      body: Container(
+        color: Colors.lightBlue[50], // Warna latar belakang halaman
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _usernameController,
-                decoration: InputDecoration(labelText: 'Username'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Harap masukkan username';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                        _isObscure ? Icons.visibility : Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        _isObscure = !_isObscure;
-                      });
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Menambahkan gambar di atas form
+            Image.asset(
+              'assets/login_image.png', // Ganti dengan path gambar Anda
+              height: 150, // Atur tinggi gambar
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20), // Jarak antara gambar dan form
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      filled: true,
+                      fillColor: Colors.white, // Warna latar belakang TextField
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Harap masukkan username';
+                      }
+                      return null;
                     },
                   ),
-                ),
-                obscureText: _isObscure,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Harap masukkan password';
-                  }
-                  return null;
-                },
+                  SizedBox(height: 16), // Jarak antara TextField
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                            _isObscure ? Icons.visibility : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure = !_isObscure;
+                          });
+                        },
+                      ),
+                    ),
+                    obscureText: _isObscure,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Harap masukkan password';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _login,
+                    child: Text('Login'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // Warna tombol Login
+                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => RegisterPage()),
+                      );
+                    },
+                    child: Text(
+                      'Belum punya akun? Daftar',
+                      style: TextStyle(color: Colors.blue), // Warna teks
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _login,
-                child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _isObscure = true;
+
+  void _register() {
+    if (_formKey.currentState!.validate()) {
+      String username = _usernameController.text;
+      String password = _passwordController.text;
+      String confirmPassword = _confirmPasswordController.text;
+
+      // Validasi password
+      if (password != confirmPassword) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Password tidak cocok')),
+        );
+        return;
+      }
+
+      // Simulasi registrasi
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Registrasi berhasil untuk $username')),
+      );
+
+      // Navigasi kembali ke halaman login
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register Page'),
+        backgroundColor: Colors.blueAccent, // Warna latar belakang AppBar
+      ),
+      body: Container(
+        color: Colors.lightBlue[50], // Warna latar belakang halaman
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Menambahkan gambar di atas form
+            Image.asset(
+              'assets/register_image.png', // Ganti dengan path gambar Anda
+              height: 150, // Atur tinggi gambar
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20), // Jarak antara gambar dan form
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      filled: true,
+                      fillColor: Colors.white, // Warna latar belakang TextField
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Harap masukkan username';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16), // Jarak antara TextField
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                            _isObscure ? Icons.visibility : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure = !_isObscure;
+                          });
+                        },
+                      ),
+                    ),
+                    obscureText: _isObscure,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Harap masukkan password';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16), // Jarak antara TextField
+                  TextFormField(
+                    controller: _confirmPasswordController,
+                    decoration: InputDecoration(
+                      labelText: 'Konfirmasi Password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Harap konfirmasi password';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _register,
+                    child: Text('Daftar'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // Warna tombol Daftar
+                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Sudah punya akun? Login',
+                      style: TextStyle(color: Colors.blue), // Warna teks
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
