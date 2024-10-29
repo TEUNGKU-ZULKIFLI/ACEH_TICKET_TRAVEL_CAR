745841e119bc26ecef5228365d664692298030eb
rahmathidayattrkj <rahmathidayath652@gmail.com>
2024-10-29 14:46:25 +0700

    rahmat mengedit tampilan profile_screen.dart

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  // Data profil pengguna
   String _name = 'Rahmat';
   String _email = 'rahmat@example.com';
   String _phoneNumber = '+62 812-3456-7890';
@@ -15,6 +16,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   DateTime _joinDate = DateTime.now();
   String _accountStatus = 'Aktif';
 
+  // Controller untuk TextField
   late TextEditingController _nameController;
   late TextEditingController _emailController;
   late TextEditingController _phoneController;
@@ -24,6 +26,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   void initState() {
     super.initState();
+    // Inisialisasi controller dengan data awal
     _nameController = TextEditingController(text: _name);
     _emailController = TextEditingController(text: _email);
     _phoneController = TextEditingController(text: _phoneNumber);
@@ -31,21 +34,34 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   void dispose() {
+    // Pastikan untuk membuang controller saat tidak lagi digunakan
     _nameController.dispose();
     _emailController.dispose();
     _phoneController.dispose();
     super.dispose();
   }
 
+  // Simpan perubahan yang dibuat
   void _saveChanges() {
+    if (_nameController.text.isEmpty ||
+        _emailController.text.isEmpty ||
+        _phoneController.text.isEmpty) {
+      // Tampilkan snackbar jika ada field yang kosong
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Semua field harus diisi!')),
+      );
+      return;
+    }
+
     setState(() {
       _name = _nameController.text;
       _email = _emailController.text;
       _phoneNumber = _phoneController.text;
-      _isEditing = false;
+      _isEditing = false; // Kembali ke mode tampilan
     });
   }
 
+  // Ubah tanggal bergabung
   void _changeJoinDate() async {
     final DateTime? picked = await showDatePicker(
       context: context,
@@ -55,7 +71,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
     if (picked != null && picked != _joinDate) {
       setState(() {
-        _joinDate = picked;
+        _joinDate = picked; // Perbarui tanggal bergabung
       });
     }
   }
@@ -65,7 +81,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profil Akun'),
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.indigoAccent,
       ),
       body: Stack(
         children: [
@@ -73,133 +89,87 @@ class _ProfileScreenState extends State<ProfileScreen> {
             padding: const EdgeInsets.all(16.0),
             child: ListView(
               children: [
-                Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+                Center(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/profile_picture.png'),
+                    backgroundColor: Colors.indigo.shade100,
                   ),
+                ),
+                const SizedBox(height: 20),
+                // Kartu untuk menampilkan data profil
+                Card(
+                  elevation: 6,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  color: Colors.indigo.shade50,
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Row(
-                          children: [
-                            const Text('Nama:', style: TextStyle(fontSize: 18)),
-                            const SizedBox(width: 8),
-                            Expanded(
-                              child: _isEditing
-                                  ? TextField(
-                                      controller: _nameController,
-                                      decoration: const InputDecoration(
-                                          border: OutlineInputBorder(),
-                                          labelText: 'Edit Nama'),
-                                    )
-                                  : Text(
-                                      _name,
-                                      style: const TextStyle(
-                                          fontSize: 16, color: Colors.grey),
-                                    ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8),
-                        Row(
-                          children: [
-                            const Text('Email:',
-                                style: TextStyle(fontSize: 18)),
-                            const SizedBox(width: 8),
-                            Expanded(
-                              child: _isEditing
-                                  ? TextField(
-                                      controller: _emailController,
-                                      decoration: const InputDecoration(
-                                          border: OutlineInputBorder(),
-                                          labelText: 'Edit Email'),
-                                    )
-                                  : Text(
-                                      _email,
-                                      style: const TextStyle(
-                                          fontSize: 16, color: Colors.grey),
-                                    ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8),
-                        Row(
-                          children: [
-                            const Text('Nomor Handphone:',
-                                style: TextStyle(fontSize: 18)),
-                            const SizedBox(width: 8),
-                            Expanded(
-                              child: _isEditing
-                                  ? TextField(
-                                      controller: _phoneController,
-                                      decoration: const InputDecoration(
-                                          border: OutlineInputBorder(),
-                                          labelText: 'Edit Nomor'),
-                                    )
-                                  : Text(
-                                      _phoneNumber,
-                                      style: const TextStyle(
-                                          fontSize: 16, color: Colors.grey),
-                                    ),
-                            ),
-                          ],
-                        ),
+                        _buildEditableRow(
+                            'Nama:', _nameController, _name, Icons.person),
+                        const SizedBox(height: 12),
+                        _buildEditableRow(
+                            'Email:', _emailController, _email, Icons.email),
+                        const SizedBox(height: 12),
+                        _buildEditableRow('Nomor HP:', _phoneController,
+                            _phoneNumber, Icons.phone),
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(height: 16),
+                const SizedBox(height: 20),
+                // Kartu untuk menampilkan informasi tambahan
                 Card(
-                  elevation: 4,
+                  elevation: 6,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+                    borderRadius: BorderRadius.circular(16),
                   ),
+                  color: Colors.indigo.shade50,
                   child: Column(
                     children: [
                       ListTile(
-                        title: const Text('Tanggal Bergabung:'),
+                        title: const Text('Tanggal Bergabung:',
+                            style: TextStyle(fontSize: 16)),
                         subtitle: Text(
-                            '${_joinDate.day}/${_joinDate.month}/${_joinDate.year}'),
+                          '${_joinDate.day}/${_joinDate.month}/${_joinDate.year}',
+                          style: const TextStyle(color: Colors.black54),
+                        ),
                         trailing: _isEditing
                             ? IconButton(
                                 icon: const Icon(Icons.calendar_today,
-                                    color: Colors.blueAccent),
+                                    color: Colors.indigo),
                                 onPressed: _changeJoinDate,
                               )
                             : null,
                       ),
                       ListTile(
-                        title: const Text('Status Akun:'),
-                        subtitle: Text(_accountStatus),
+                        title: const Text('Status Akun:',
+                            style: TextStyle(fontSize: 16)),
+                        subtitle: Text(_accountStatus,
+                            style: TextStyle(color: Colors.green)),
                       ),
                     ],
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Center(
-                  child: const CircleAvatar(
-                    radius: 50,
-                    backgroundImage:
-                        AssetImage('assets/img/profile_picture.ico'),
                   ),
                 ),
               ],
             ),
           ),
+          // Tombol untuk mengedit atau menyimpan
           Positioned(
             left: 16,
             bottom: 16,
             child: FloatingActionButton.extended(
-              backgroundColor: _isEditing ? Colors.green : Colors.blueAccent,
+              backgroundColor: _isEditing ? Colors.green : Colors.indigoAccent,
               onPressed: () {
                 setState(() {
                   if (_isEditing) {
-                    _saveChanges();
+                    _saveChanges(); // Simpan perubahan jika dalam mode edit
                   } else {
-                    _isEditing = true;
+                    _isEditing = true; // Masuk ke mode edit
                   }
                 });
               },
@@ -209,6 +179,38 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
         ],
       ),
+    );
+  }
+
+  // Fungsi untuk membangun baris editable dengan ikon
+  Widget _buildEditableRow(String label, TextEditingController controller,
+      String value, IconData icon) {
+    return Row(
+      children: [
+        Icon(icon, color: Colors.blue), // Menambahkan ikon
+        const SizedBox(width: 8),
+        Text(label,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
+        const SizedBox(width: 8),
+        Expanded(
+          child: _isEditing
+              ? TextField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                  style: const TextStyle(fontSize: 16),
+                )
+              : Text(
+                  value,
+                  style: const TextStyle(fontSize: 16, color: Colors.black54),
+                ),
+        ),
+      ],
     );
   }
 }
